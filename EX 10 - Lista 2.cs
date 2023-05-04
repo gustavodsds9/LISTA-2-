e5d2d1b8d99d379baf50834bb343298697b7fa22
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-05-03 23:55:58 -0300

    Add files via upload

---
+++ EX 10 - Lista 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex10Lis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1;
+            double nota2;
+            double media;
+
+            Console.Write("Digite a 1º Nota: ");
+            nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite a 2º Nota: ");
+            nota2 = double.Parse(Console.ReadLine());
+            media = (nota1 + 2 * nota2) / 3;
+
+            if (media < 5)
+                Console.WriteLine("Aluno(a) Reprovado! :( ");
+            else
+                Console.WriteLine("Aluno(a) Aprovado! ;) ");
+        }
+    }
+}
