970b8a085d12b42feb79b7f04e7af5b0b7777368
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-05-03 23:56:08 -0300

    Add files via upload

---
+++ EX 11 - Lista 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex11Lis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1;
+            double p2;
+
+            Console.Write("Digite a Nota P1: ");
+            p1 = double.Parse(Console.ReadLine());
+
+            p2 = (3 * 5 - p1) / 2;
+
+            if (p2 <= 10)
+                Console.WriteLine("Aluno(a) Precisa Tirar uma Nota {0} ou Maior na P2 para ser Aprovado de ano!", p2);
+            else
+                Console.WriteLine("Idependente da Nota do Aluno esse estar Reprovado! :0 ");
+        }
+    }
+}
