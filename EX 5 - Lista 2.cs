9605761f61f18266edf4fd5c2d587b0f660acd89
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-05-03 23:54:36 -0300

    Add files via upload

---
+++ EX 5 - Lista 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex5Lis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int h;
+            int a;
+
+            Console.Write("Digite o Valor da Base: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Digite o Valor da altura: ");
+            h = int.Parse(Console.ReadLine());
+            a = b * h;
+
+            if (a > 100)
+                Console.WriteLine("Terreno Grande :) ");
+            else
+                Console.WriteLine("Terreno Pequeno :( ");
+        }
+    }
+}
