f02f7918e28f38ce2c285511df56d9dd3e870699
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-05-03 23:55:35 -0300

    Add files via upload

---
+++ EX 8 - Lista 2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex8Lis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double l1;
+            double l2;
+            double l3;
+            double h1;
+            double h2;
+            double h3;
+
+            Console.Write("Digite o Valor da 1º Lateral: ");
+            l1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite o Valor da 2º Lateral: ");
+            l2 = double.Parse(Console.ReadLine());
+            Console.Write("Digite o Valor da 3º Lateral: ");
+            l3 = double.Parse(Console.ReadLine());
+
+            h1 = Math.Sqrt(Math.Pow(l2, 2) + Math.Pow(l3, 2));
+            h2 = Math.Sqrt(Math.Pow(l1, 2) + Math.Pow(l3, 2));
+            h3 = Math.Sqrt(Math.Pow(l1, 2) + Math.Pow(l2, 2));
+
+            if ((h1 == l1) || (h2 == l2) || (h3 == l3))
+                Console.WriteLine("Os Valores Informados para os Lados, Formam um Triângulo Retângulo!");
+
+            else
+                Console.WriteLine("Os Valores Informados para os Lados, Não Formam um Triângulo Retângulo! :(");
+        }
+    }
+}
