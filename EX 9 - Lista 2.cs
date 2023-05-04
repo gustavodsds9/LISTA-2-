09bec1276d55724806068b65a40a973b3fa3c0f6
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-05-03 23:55:46 -0300

    Add files via upload

---
+++ EX 9 - Lista 2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex9Lis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double p;
+            double h;
+            double imc;
+            int s;
+
+            Console.Write("Digite seu Peso: ");
+            p = double.Parse(Console.ReadLine());
+            Console.Write("Digite sua Altura: ");
+            h = double.Parse(Console.ReadLine());
+            Console.Write("Digte seu Sexo, sendo (1) para Masculino, e (2) para Feminino: ");
+            s = int.Parse(Console.ReadLine());
+            imc = p / (Math.Pow(h, 2));
+
+            if (s == 1)
+                if (imc < 20)
+                    Console.WriteLine("Abaixo do Peso! :| ");
+                else
+                       if (imc < 25)
+                    Console.WriteLine("Peso Ideal! :) ");
+                else
+                    Console.WriteLine("Acima do Peso! :( ");
+
+            else
+                    if (imc < 19)
+                Console.WriteLine("Abaixo do Peso! :| ");
+            else
+                    if (imc < 24)
+                Console.WriteLine("Peso Ideal! :) ");
+            else
+                Console.WriteLine("Acima do Peso! :( ");
+        }
+    }
+}
