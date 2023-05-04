fa45f135f5457125d7b77ce762edd77a53ed6cc7
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-05-03 23:55:23 -0300

    Add files via upload

---
+++ EX 7 - Lista 2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex7Lis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double v1;
+            double v2;
+            double v3;
+
+            Console.Write("Digite o Valor da 1º Lateral: ");
+            v1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite o Valor da 3º Lateral: ");
+            v2 = double.Parse(Console.ReadLine());
+            Console.Write("Digite o Valor da 3º Lateral: ");
+            v3 = double.Parse(Console.ReadLine());
+
+            if ((v1 > v2 + v3) || (v2 > v1 + v3) || (v3 > v1 + v2))
+                if ((v1 == v2) && (v1 == v3))
+                    Console.WriteLine("Os Lados Formam um Triângulo Equilátero! ");
+                else
+                    if ((v1 == v2 && v1 < v3) || (v1 == v3 && v1 < v2) || (v3 == v2 && v3 < v1))
+                    Console.WriteLine("Os Lados Formam um Triângulo Isósceles! ");
+                else
+                    Console.WriteLine("Os Lados Formam um Triângulo Escaleno! ");
+            else
+                Console.WriteLine("Os Valores Informados para os Lados, não Formam um Triângulo !!!");
+        }
+    }
+}
