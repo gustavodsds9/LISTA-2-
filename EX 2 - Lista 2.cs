ca234cd69144e46b9f841c210179528955381650
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-05-03 23:50:43 -0300

    Add files via upload

---
+++ EX 2 - Lista 2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2Lis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1;
+            int v2;
+
+            Console.Write("Informe o 1º Valor: ");
+            v1 = int.Parse(Console.ReadLine());
+            Console.Write("Informe o 2º Valor: ");
+            v2 = int.Parse(Console.ReadLine());
+
+            if (v1 == v2)
+                Console.WriteLine("Ambos Valores são Iguais!");
+            else
+                if (v1 > v2)
+                Console.WriteLine("O 1º Valor ({0}) é Maior que o 2º Valor ({1})", v1, v2);
+            else
+                Console.WriteLine("O 2º Valor ({1}) é Maior que o 1º Valor ({0})", v1, v2);
+
+        }
+    }
+}
