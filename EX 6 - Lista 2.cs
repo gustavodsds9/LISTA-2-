3d29237906d16443e21119b031f74a0fb95da0a1
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-05-03 23:55:00 -0300

    Add files via upload

---
+++ EX 6 - Lista 2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex6Lis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double p;
+            double h;
+            double imc;
+
+            Console.Write("Digite o seu Peso: ");
+            p = double.Parse(Console.ReadLine());
+            Console.Write("Digite sua Altura: ");
+            h = double.Parse(Console.ReadLine());
+            imc = p / (Math.Pow(h, 2));
+
+            if (imc < 20)
+                Console.WriteLine("Abaixo do peso :| ");
+            else
+                    if (imc < 25)
+                Console.WriteLine("Peso Ideal :) ");
+            else
+                Console.WriteLine("Acima do Peso :( ");
+        }
+    }
+}
